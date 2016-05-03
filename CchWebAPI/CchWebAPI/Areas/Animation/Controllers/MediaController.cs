50c9f4973fea6fa75f9e26c08c824c5314b77f5b
Rey David <rdavid@clearcosthealth.com>
2016-05-03 16:55:26 -0700

    PC-750

--- CchWebAPI/CchWebAPI/Areas/Animation/Controllers/MediaController.cs
+++ CchWebAPI/CchWebAPI/Areas/Animation/Controllers/MediaController.cs
@@ -22,11 +22,34 @@ namespace CchWebAPI.Areas.Animation.Controllers
                 {
                     int employerId = Request.EmployerID();
                     int cchId = Request.CCHID();
+
                     string mediaBaseAddress = "MediaBaseAddress".GetConfigurationValue();
 
                     string mediaUrl = string.Format("{0}/?cid={1}|{2}|{3}|{4}",
                         mediaBaseAddress, employerId, campaignId, contentId, cchId);
 
+                    using (GetEmployerConnString gecs = new GetEmployerConnString(employerId))
+                    {
+                        using (GetUserContent guc = new GetUserContent())
+                        {
+                            guc.CampaignId = campaignId;
+                            guc.BaseContentId = contentId;
+                            guc.PagesSize = 1;
+
+                            guc.CchId = cchId;
+                            guc.GetData(gecs.ConnString);
+
+                            if (guc.Results.Count == 1)
+                            {
+                                string contentType = guc.Results[0].ContentType;
+
+                                if (contentType.Equals("Vimeo"))
+                                {
+                                    mediaUrl = guc.Results[0].ContentUrl;
+                                }
+                            }
+                        }
+                    }
                     data.MediaUrl = mediaUrl;
                     hrm = Request.CreateResponse(HttpStatusCode.OK, (object) data);
                 }
