6b0297a6d16d4ac908160d66fb95ba05545bfdbe
Rey David <rdavid@clearcosthealth.com>
2016-09-01 08:38:32 -0700

    MPM-1370

---
+++ CchWebAPI/CchWebAPI/Areas/Animation/Models/Dependent.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CchWebAPI.Areas.Animation.Models
+{
+    /// <summary>
+    /// A person who is a dependent of an employee
+    /// </summary>
+    [Serializable]
+    public class Dependent
+    {
+        public Dependent()
+        {
+        }
+
+        private int cchID = -1;
+        private string fullName = string.Empty;
+        private int age = -1;
+        private string email = string.Empty;
+        private Boolean isAdult = false;
+        private string relationshipText = string.Empty;
+        private string subscriberMedicalId = string.Empty;
+
+        public int CCHID
+        {
+            get { return cchID; }
+            set { cchID = value; }
+        }
+
+        public string SubscriberMedicalId
+        {
+            get { return subscriberMedicalId; }
+            set { subscriberMedicalId = value; }
+        }
+        public string FullName
+        {
+            get
+            {
+                return fullName;
+            }
+            set { fullName = value; }
+        }
+
+        public string Email
+        {
+            get { return email; }
+            set { email = value; }
+        }
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+        public Boolean IsAdult { get { return isAdult; } set { isAdult = Boolean.Parse(value.ToString()); } }
+        public string RelationshipText { get { return relationshipText; } set { relationshipText = value; } }
+    }
+}
---
+++ CchWebAPI/CchWebAPI/Areas/Animation/Models/Dependents.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Data;
+
+namespace CchWebAPI.Areas.Animation.Models
+{
+    [Serializable]
+    public class Dependents : CollectionBase
+    {
+        public Dependents()
+        {
+        }
+
+
+        public Dependent this[int index]
+        {
+            get { return (Dependent)List[index]; }
+        }
+
+        public void Add(Dependent dependent)
+        {
+            List.Add(dependent);
+        }
+
+        public Dependents GetAdultDependents()
+        {
+            Dependents adults = new Dependents();
+
+            foreach (Dependent dep in List)
+            {
+                if (dep.Age >= 18)
+                {
+                    adults.Add(dep);
+                }
+            }
+            return adults;
+        }
+
+        public DataTable AsDataTable()
+        {
+            DataTable dependentData = new DataTable("DependentData");
+
+            dependentData.Columns.Add("FullName");
+            dependentData.Columns.Add("FirstName");
+            dependentData.Columns.Add("LastName");
+            dependentData.Columns.Add("RelationShip");
+            dependentData.Columns.Add("ShowQuestions");
+            dependentData.Columns.Add("DepToUser");
+            dependentData.Columns.Add("UserToDep");
+            dependentData.Columns.Add("Adult");
+            dependentData.Columns.Add("CCHID");
+
+            DataRow dependentRow = null;
+            foreach (Dependent dependent in List)
+            {
+                dependentRow = dependentData.NewRow();
+                dependentRow["FullName"] = dependent.FullName;
+                dependentRow["RelationShip"] = dependent.RelationshipText;
+                dependentRow["Adult"] = dependent.IsAdult;
+                dependentRow["CCHID"] = dependent.CCHID;
+                dependentData.Rows.Add(dependentRow);
+            }
+
+            return dependentData;
+        }
+    }
+}
