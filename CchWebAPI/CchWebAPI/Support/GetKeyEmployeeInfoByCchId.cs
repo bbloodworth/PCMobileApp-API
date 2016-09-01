943c5f7e9e3089160da886d11dfe83a3df7d461a
Rey David <rdavid@clearcosthealth.com>
2016-09-01 08:41:48 -0700

    MPM-1370

--- CchWebAPI/CchWebAPI/Support/GetKeyEmployeeInfoByCchId.cs
+++ CchWebAPI/CchWebAPI/Support/GetKeyEmployeeInfoByCchId.cs
@@ -9,6 +9,7 @@ namespace CchWebAPI.Support
 
         private DataTable EmployeeTable { get { return Tables[0] ?? new DataTable("Empty"); } }
         private DataRow EmployeeRow { get { return EmployeeTable.Rows[0] ?? EmployeeTable.NewRow(); } }
+        public DataTable DependentTable { get { return ( Tables.Count > 1 ? Tables[1] : new DataTable("EmptyTable")); } }
         private object this[String ColName] { get { return EmployeeRow[ColName]; } }
 
         public String CnxString { get { return this["ConnectionString"].ToString(); } }
@@ -16,5 +17,8 @@ namespace CchWebAPI.Support
         public GetKeyEmployeeInfoByCchId()
             : base("GetKeyEmployeeInfoByCchId")
         { Parameters.New("CCHID", SqlDbType.Int); }
+
+        public void ForEachDependent<DataRow>(Action<DataRow> action)
+        { foreach (DataRow dr in DependentTable.Rows) action(dr); }
     }
 }
