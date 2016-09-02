e6fd03d8a381268c99a13912d213caeec8b62c91
Rey David <rdavid@clearcosthealth.com>
2016-09-02 16:07:22 -0700

    MPM-1370

---
+++ CchWebAPI/CchWebAPI/Services/EmployeeServices.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using ClearCost.Data;
+using System.Data;
+using Dapper;
+using CchWebAPI.Areas.Animation.Models;
+using System;
+
+namespace CchWebAPI.Services
+{
+    public class EmployeeServices
+    {
+        public MemberDetail GetKeyEmployeeInfo(int employerId, int cchId)
+        {
+            //var keyEmployeeInfo = DataGateway.Query(
+            //    "GetKeyEmployeeInfoByCchId", 
+            //    new
+            //    {
+            //        CCHID = cchId
+            //    },
+            //    DataSource.Employer,
+            //    employerId
+            //);
+            //return keyEmployeeInfo;
+
+            MemberDetail keyEmployeeInfo = null;
+
+            using (var connection = ConnectionFactory.Get(DataSource.Employer, employerId))
+            {
+                try
+                {
+                    connection.Open();
+                    var multiple = connection.QueryMultiple(
+                        "GetKeyEmployeeInfoByCchId",
+                        new
+                        {
+                            CCHID = cchId
+                        },
+                        commandType: CommandType.StoredProcedure);
+
+                    keyEmployeeInfo = multiple.Read<MemberDetail>().AsList().ToArray()[0];
+                    keyEmployeeInfo.Dependents.AddRange(multiple.Read<DependentDetail>());
+                }
+                catch (Exception ex)
+                {
+                    DataGateway.LogError(ex);
+                }
+                finally
+                {
+                    if (connection.State != ConnectionState.Closed)
+                    {
+                        connection.Close();
+                    }
+                }
+            }
+            return keyEmployeeInfo;
+        }
+    }
+}
