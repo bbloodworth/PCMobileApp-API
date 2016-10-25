3608c44f1d9b9f531dc7b6041fc445b3115f5a96
Rey David <rdavid@clearcosthealth.com>
2016-10-25 15:24:19 -0700

    MPM-1631

---
+++ CchWebAPI/CchWebAPI/BenefitContributions/Models/ContributionType.cs
@@ -0,0 +1,20 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace CchWebAPI.BenefitContributions.Models
+{
+    public class ContributionType
+    {
+        public int ContributionTypeKey { get; set; }
+        public string ContributionTypeCode { get; set; }
+        public string ContributionTypeName { get; set; }
+
+        public class ConfigurationTypeConfiguration: EntityTypeConfiguration<ContributionType>
+        {
+            public ConfigurationTypeConfiguration()
+            {
+                ToTable("ContributionType_d");
+                HasKey(k => k.ContributionTypeKey);
+            }
+        }
+    }
+}
---
+++ CchWebAPI/CchWebAPI/BenefitContributions/Models/EmployeeMember.cs
@@ -0,0 +1,24 @@
+﻿using System.Data.Entity.ModelConfiguration;
+
+namespace CchWebAPI.BenefitContributions.Models
+{
+    public class EmployeeMember
+    {
+        public int EmployeeKey { get; set; }
+        public int CCHID { get; set; }
+        public string SourceEmployeeNum { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public string PayGroupCode { get; set; }
+        public string PayGroupName { get; set; }
+
+        public class EmployeeMemberConfiguration: EntityTypeConfiguration<EmployeeMember>
+        {
+            public EmployeeMemberConfiguration()
+            {
+                ToTable("Employee_d");
+                HasKey(k => k.EmployeeKey);
+            }
+        }
+    }
+}
