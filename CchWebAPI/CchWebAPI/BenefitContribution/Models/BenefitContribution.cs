aa72f84ee1e54c207fa1d525a6b4150273d4c020
Benjamin Bloodworth <bbloodworth@clearcosthealth.com>
2016-12-09 17:40:31 -0500

    +Changed to use BenefitTypeCode instead of BeneftPlanTypeCode.

--- CchWebAPI/CchWebAPI/BenefitContribution/Models/BenefitContribution.cs
+++ CchWebAPI/CchWebAPI/BenefitContribution/Models/BenefitContribution.cs
@@ -8,32 +8,32 @@ using System.Web;
 namespace CchWebAPI.BenefitContribution.Models
 {
     public class BenefitContribution {
-        public string PayerName;
-        public string BenefitTypeName;
-        public DateTime PayrollFileReceivedDate;
-        public List<BenefitContributionDetail> BenefitContributions;
-        public List<PercentageElected> PercentageElected;
+        public string PayerName { get; set; }
+        public string BenefitTypeName { get; set; }
+        public DateTime PayrollFileReceivedDate { get; set; }
+        public List<BenefitContributionDetail> BenefitContributions { get; set; }
+        public List<PercentageElected> PercentageElected { get; set; }
     }
 
     public class BenefitContributionDetail
     {
         [JsonProperty("EmployeeId")]
-        public int CchId;
-        public string EmployeeFirstName;
-        public string EmployeeLastName;
-        public DateTime AsOfDate;
-        public DateTime DWCreateDate;
-        public string PayrollCategoryName;
-        public string PayrollMetricName;
-        public bool PreTaxInd;
-        public string ContributionTypeCode;
-        public string ContributionTypeName;
-        public decimal? PerPeriodAmt;
-        public decimal? YTDAmt;
-        public string ReportingCategoryCode;
+        public int CchId { get; set; }
+        public string EmployeeFirstName { get; set; }
+        public string EmployeeLastName { get; set; }
+        public DateTime AsOfDate { get; set; }
+        public DateTime DWCreateDate { get; set; }
+        public string PayrollCategoryName { get; set; }
+        public string PayrollMetricName { get; set; }
+        public bool PreTaxInd { get; set; }
+        public string ContributionTypeCode { get; set; }
+        public string ContributionTypeName { get; set; }
+        public decimal? PerPeriodAmt { get; set; }
+        public decimal? YTDAmt { get; set; }
+        public string ReportingCategoryCode { get; set; }
         [JsonProperty("IsCurrent")]
-        public bool CurrentPayPeriodInd;
-        public string PayrollMetricDisplayName;
+        public bool CurrentPayPeriodInd { get; set; }
+        public string PayrollMetricDisplayName { get; set; }
 
 
         public class BenefitContributionDetailConfiguration : EntityTypeConfiguration<BenefitContributionDetail> {
@@ -44,11 +44,11 @@ namespace CchWebAPI.BenefitContribution.Models
 
     public class PercentageElected {
         [JsonIgnore]
-        public int CCHID;
+        public int CCHID { get; set; }
         [JsonIgnore]
-        public string BenefitPlanTypeCode;
-        public string ContributionName;
-        public float? Percentage;
+        public string BenefitPlanTypeCode { get; set; }
+        public string ContributionName { get; set; }
+        public float? Percentage { get; set; }
     }
 
 
