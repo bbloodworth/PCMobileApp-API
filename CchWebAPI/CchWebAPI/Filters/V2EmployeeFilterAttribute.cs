b0830057936fb5a902114b37527ec10357dd731f
Benjamin Bloodworth <bbloodworth@clearcosthealth.com>
2016-12-20 20:28:51 -0500

    +Modified all controllers to return IHttpActionResult for better status code handling. +Added filter to automatically set Employer object based on logged in user's Request.EmployerId(). +Added filter to verify that CchId in url route is either the logged in user (Request.CCHID()) or a dependent of the logged in user. +Modified user login to submit 270/271 requests for all of the loggedin user's dependents. +Added repository query to retrieve employee dependents across all plans. +Fixed bug in route to get plan members to grab all dependents not just the subscriber.

---
+++ CchWebAPI/CchWebAPI/Filters/V2EmployeeFilterAttribute.cs
@@ -0,0 +1,50 @@
+﻿using CchWebAPI.Employee.Dispatchers;
+using CchWebAPI.Employee.Models;
+using ClearCost.Platform;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+using System.Web.Http.Filters;
+
+namespace CchWebAPI.Filters {
+    public class V2EmployeeFilterAttribute : ActionFilterAttribute {
+        public override void OnActionExecuting(HttpActionContext actionContext) {
+            base.OnActionExecuting(actionContext);
+
+            // The cchId in the url needs to match the cchId in the Request or one of their dependents.
+            // Retrieve employee dependents
+            var employer = EmployerCache.Employers.FirstOrDefault(e =>
+                        e.Id == actionContext.Request.EmployerID());
+
+            var repository = new Employee.Data.V2.EmployeeRepository();
+            var members = new List<PlanMember>();
+
+            var dispatcher = new EmployeeDispatcher(repository);
+            Task.Run(async () => members = await dispatcher.GetEmployeeDependentsAsync(
+                            employer,
+                            actionContext.Request.CCHID())).Wait();
+
+            // Verify that the requested cchId is viewable by the logged in cchId.
+            int requestedCchId = 0;
+            bool validCchIdRequest = false;
+
+            Int32.TryParse(actionContext.ActionArguments["cchId"].ToString(), out requestedCchId);
+
+            foreach(var member in members) {
+                if (member.CchId == requestedCchId) {
+                    validCchIdRequest = true;
+                    break;
+                }
+            }
+
+            if (!validCchIdRequest) {
+                throw new HttpResponseException(HttpStatusCode.Forbidden);
+            }
+        }
+    }
+}
